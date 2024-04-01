90806165f30d07ec29bce9ffc2feeceff28667ed
jang <treous11@gmail.com>
2024-04-01 09:26:58 +0900

    init

--- src/loop/quiz/Quiz9.java
+++ src/loop/quiz/Quiz9.java
@@ -4,9 +4,9 @@ public class Quiz9 {
 
 	public static void main(String[] args) {
 		
-		for(int i = 1; i <= 3; i++) {
+		for(int i = 1; i <= 3; i++) {  // 외부는 높이
 			
-			for(int j = 1; j <= 5; j++) {
+			for(int j = 1; j <= 5; j++) { // 내부는 길이
 				System.out.print("*");
 			}
 			System.out.println();
